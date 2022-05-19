2fbf906d8bf2f85e11f88bc6335a21eeba84a043
mitsuki-lc <102834279+mitsuki-lc@users.noreply.github.com>
2022-05-18 21:52:15 -0300

    uso de camera

--- app/src/main/java/com/example/divulgacap/MainActivity.kt
+++ app/src/main/java/com/example/divulgacap/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.divulgacap
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var Buscar: Button
     private lateinit var Sobre: Button
     private lateinit var divulgaoseuaqui: Button
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,11 +50,27 @@ class MainActivity : AppCompatActivity() {
         })
 
         divulgaoseuaqui.setOnClickListener(View.OnClickListener {
-            divulgaoseuaquiActivity()
+            dispatchTakePictureIntent()
         })
 
 
 
+    }
+
+    private fun dispatchTakePictureIntent () {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { TakePictureIntent ->
+            TakePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(TakePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val imageView: ImageView = findViewById(R.id.imageView)
+            imageView.setImageBitmap(imageBitmap)
+
+        }
     }
             fun EntrarActivity() {
                 startActivity(Intent(this,tela1::class.java))
@@ -58,11 +78,6 @@ class MainActivity : AppCompatActivity() {
             fun BuscarActivity() {
                 startActivity(Intent(this,tela2::class.java));
             }
-
-            fun divulgaoseuaquiActivity() {
-                startActivity(Intent(this,tela4::class.java));
-            }
-
 
 
 
